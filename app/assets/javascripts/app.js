f1b9dda2f5656b71844499c15c51b5a1e6b7e6e7
benser1 <briganenser28@gmail.com>
2016-03-15 08:56:46 -0700

    dislaying info from API

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,9 +1,49 @@
-angular.module('countryInfo', [])
+var app = angular.module('countryInfo', [])
+
+// Factory
+
+  app.factory('CountryFactory', function($http) {
+    var data = {};
+    var url = "https://restcountries-v1.p.mashape.com/name/switzerland";
+
+    data.LoadCountry = function() {
+      return $http.get(url, {
+        headers: {
+          "X-Mashape-Key" : "bd7d3zQHwRmshUFZqeaTpuFT3Nv7p1Rz6nCjsnmGF4IbZLJoKy"
+        }
+      });
+    }
+    return data
+   });
+
 
 //Controller 
 
-.controller('CountryController', [
-'$scope',
-function($scope){
-  $scope.test = 'Hello world!';
-}]);
+  app.controller('CountryController', ['$scope', 'CountryFactory', function($scope, CountryFactory){
+    $scope.country;
+    // debugger;
+
+    CountryFind();
+
+    function CountryFind() {
+      CountryFactory
+        .LoadCountry()
+        .success(function(data) {
+          $scope.country = data;
+        })
+        .error(function(data, status, header, config) {
+          console.log(data);
+          console.log(status);
+          console.log(headers);
+          console.log(config);
+        });
+    }
+  }]);
+
+  // unirest.get("https://restcountries-v1.p.mashape.com/name/switzerland")
+  //   .header("X-Mashape-Key", "bd7d3zQHwRmshUFZqeaTpuFT3Nv7p1Rz6nCjsnmGF4IbZLJoKy")
+  //   .header("Accept", "text/plain")
+  //   .end(function (result) {
+  //     console.log(result.status, result.headers, result.body);
+  //   });
+
